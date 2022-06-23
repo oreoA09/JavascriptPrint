033f7d2064482d54061746f8faf2af11cdae3a8d
oreoA09 <oreo.akanni@gmail.com>
2022-06-23 19:12:19 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName, myHeight, myCountry;
+
+myName = "Oreo";
+myHeight = "5.9inches";
+myCountry = "Ghana";
+
+console.log("My name is " + myName + ". Am " + myHeight + " tall and I live in " + myCountry + ".");
